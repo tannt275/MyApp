06f23d7d3f3d4cfc8f1c40a17626f28ef4e1118f
tannt275 <tannt275@gmail.com>
2016-02-24 10:51:29 +0700

    add fragment first

--- main/java/tannt275/reuseactionbrain/ui/TutorialActivity.java
+++ main/java/tannt275/reuseactionbrain/ui/TutorialActivity.java
@@ -3,18 +3,33 @@ package tannt275.reuseactionbrain.ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import tannt275.reuseactionbrain.R;
 
 public class TutorialActivity extends AppCompatActivity {
 
+    private Button bottomBtn;
+    private TextView position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        position = (TextView) findViewById(R.id.tutorial_position);
+        bottomBtn = (Button) findViewById(R.id.tutorial_button);
+    }
+
+    private void displayFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.tutorial_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
 }
