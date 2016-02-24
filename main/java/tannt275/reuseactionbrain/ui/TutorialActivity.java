9753298651d9c749e49a69bf727be91f37934f44
tannt275 <tannt275@gmail.com>
2016-02-24 17:42:47 +0700

    add start fragment

--- main/java/tannt275/reuseactionbrain/ui/TutorialActivity.java
+++ main/java/tannt275/reuseactionbrain/ui/TutorialActivity.java
@@ -1,5 +1,6 @@
 package tannt275.reuseactionbrain.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -7,13 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import java.awt.font.TextAttribute;
 
 import tannt275.reuseactionbrain.R;
 import tannt275.reuseactionbrain.common.Log;
 import tannt275.reuseactionbrain.ui.fragment.tutorial.FinishFragment;
 import tannt275.reuseactionbrain.ui.fragment.tutorial.FirstFragment;
 import tannt275.reuseactionbrain.ui.fragment.tutorial.SecondFragment;
+import tannt275.reuseactionbrain.ui.fragment.tutorial.StartFragment;
 
 public class TutorialActivity extends AppCompatActivity {
 
@@ -21,47 +22,71 @@ public class TutorialActivity extends AppCompatActivity {
 
     private Button bottomBtn;
     private static int current_position = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
         bottomBtn = (Button) findViewById(R.id.tutorial_button);
 
-        FirstFragment firstFragment = new FirstFragment();
-        displayFragment(firstFragment);
+        StartFragment startFragment = new StartFragment();
+        displayFragment(startFragment);
+
+        bottomBtn.setText("Start Tutorial");
         bottomBtn.setOnClickListener(buttonClickListener);
 
     }
+
     private View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            current_position ++;
-            Log.e(TAG, "current position: "+ current_position);
+            current_position++;
+            Log.e(TAG, "current position: " + current_position);
             bottomBtn.setEnabled(false);
             showFragmentWithPosition(current_position);
         }
     };
 
     private void showFragmentWithPosition(int current_position) {
-        if (current_position == 1){
+        if (current_position == 1) {
             bottomBtn.setEnabled(true);
+            bottomBtn.setText("Next");
+            FirstFragment firstFragment = new FirstFragment();
+            displayFragment(firstFragment);
+
+        } else if (current_position == 2) {
+            bottomBtn.setEnabled(true);
+            bottomBtn.setText("Next");
             SecondFragment secondFragment = new SecondFragment();
             displayFragment(secondFragment);
-        } else if (current_position == 2){
-            bottomBtn.setEnabled(true);
+
+        } else if (current_position == 3) {
             FinishFragment finishFragment = new FinishFragment();
             displayFragment(finishFragment);
             bottomBtn.setText("Let's Start");
+            bottomBtn.setEnabled(true);
+        } else if (current_position == 4) {
+            navigateToMain();
         } else {
-            Log.e(TAG, "out size");
+            Log.e(TAG, "out size...");
         }
     }
 
-    private void displayFragment(Fragment fragment){
+    private void navigateToMain() {
+        Intent toMain = new Intent(TutorialActivity.this, MainActivity.class);
+        startActivity(toMain);
+        finish();
+    }
+
+    private void displayFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.tutorial_container, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 
+    @Override
+    public void onBackPressed() {
+
+    }
 }
---
+++ main/java/tannt275/reuseactionbrain/ui/fragment/tutorial/StartFragment.java
@@ -0,0 +1,48 @@
+package tannt275.reuseactionbrain.ui.fragment.tutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
+
+import tannt275.reuseactionbrain.R;
+
+public class StartFragment extends Fragment {
+
+    TextView textView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_start, container, false);
+        textView = (TextView) rootView.findViewById(R.id.textStart);
+
+        final Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(3000);
+
+        final Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(3000);
+        out.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                textView.startAnimation(in);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        return rootView;
+    }
+
+}
